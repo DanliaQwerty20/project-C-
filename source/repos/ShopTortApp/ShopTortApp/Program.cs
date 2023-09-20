c2ff27925e3bb3018ec499e060556dead9b7a08e
Daniil Korchagin <danlia2000789@gmail.com>
2023-09-20 18:58:21 +0300

    create controller

--- source/repos/ShopTortApp/ShopTortApp/Program.cs
+++ source/repos/ShopTortApp/ShopTortApp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using ShopTortApp.Storage;
+using System.Net;
+using System.Text;
 
 public class Program
 {
@@ -55,22 +57,70 @@ public class Program
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
-
         app.UseHttpsRedirection();
         app.UseStaticFiles();
+
         Console.WriteLine("Program 1:");
+
         app.UseCors();
         app.UseRouting();
-
         app.UseAuthorization();
-
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        // Создаем объект HttpListener
+        HttpListener listener = new HttpListener();
+        // Указываем адрес, на котором будет слушать сервер
+        listener.Prefixes.Add("http://25.27.145.30:8080/");
+
+        try
+        {
+            // Запускаем сервер
+            listener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание запросов...");
+
+            while (true)
+            {
+                // Принимаем входящий запрос
+                HttpListenerContext context = listener.GetContext();
+
+                // Получаем объект запроса и ответа
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                using (StreamReader reader = new StreamReader(request.InputStream))
+                {
+                    string requestData = reader.ReadToEnd();
+                    Console.WriteLine($"Получен запрос: {requestData}");
+                }
+
+                string responseString = "Првет, мир!";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(responseString);
+                response.ContentType = "text/plain";
+                response.ContentLength64 = responseBytes.Length;
+
+                using (Stream output = response.OutputStream)
+                {
+                    output.Write(responseBytes, 0, responseBytes.Length);
+                }
+
+                Console.WriteLine("Ответ отправлен.");
+            }
+            }
+        catch (HttpListenerException e)
+        {
+            Console.WriteLine($"Ошибка: {e.Message}");
+        }
+        finally
+        {
+            // Останавливаем сервер
+            listener.Close();
+            listener.Stop();
+        }
 
         app.Run();
     }
